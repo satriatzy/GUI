7e9cfd30c1e47d98629fb296a39dbfa3bf94903e
SatriaTzy <45333174+satriatzy@users.noreply.github.com>
2021-02-28 15:54:39 +0800

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,11 @@
+####################################
+# First basic think learn python2021
+# Sam Achmad
+####################################
+import os
+os.system ('Clear')
+
+full_name = "My Name Sam Achmad"
+print (full_name)
+age = "My age 28 year old"
+print (age)
